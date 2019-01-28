bbcafa2194ee3d743f7cf191aff26e9314596bbe
Javi Carrion <suavejavi@google.com>
2019-01-27 20:35:53 -0500

    refactor: App.css map width

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,7 +7,6 @@ export default class Search extends Component {
         <div className="search-form">
           <span class="text">Search for Coffee Nearby</span>
           <input id="places-search" type="text" placeholder="e.g. Starbucks" />
-          <input id="go-places" type="button" value="Go" />
         </div>
       </aside>
     );
